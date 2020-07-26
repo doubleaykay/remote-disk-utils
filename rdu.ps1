a017731c90bd9e65ef0d497a9fbf88c1f3aefa84
doubleaykay <the.a.cars@gmail.com>
2020-07-26 15:25:34 -0400

    init central script

---
+++ rdu.ps1
@@ -0,0 +1,16 @@
+﻿# central script: a "main" if you will
+# passes arguments to the relevant sub-scripts
+
+if ($args[0] -eq 'clone') {
+# run clone sub-script
+
+} ElseIF ($args[0] -eq 'backup') {
+# run backup sub-script
+
+} ElseIf ($args[0] -eq 'sync') {
+# run sync sub-script
+
+} Else {
+# print help message
+
+}
